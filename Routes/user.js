4c0bdb8862ec1d205e07f8218f96ef9e3143f4c9
Anshad.MK <84131708+Anshad-mk@users.noreply.github.com>
2023-04-12 22:01:37 +0530

    seat select

--- Routes/user.js
+++ Routes/user.js
@@ -7,6 +7,7 @@ const userMode = require("../Models/userModel");
 // http://localhost:4000/findShow/
 router.post("/signup", register);
 router.post('/login',login)
+// router.post('/seatb')
 router.get('/Movielist',Movielist)
 router.get('/Movie/:id',singleMovie)
 router.post('/emailauth',emailauth)
