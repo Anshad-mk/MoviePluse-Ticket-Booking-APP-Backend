246791fcb6f5da533dbea69a443d549fb5cc9298
Anshad.MK <84131708+Anshad-mk@users.noreply.github.com>
2023-04-27 21:26:25 +0530

    hosting 24

--- index.js
+++ index.js
@@ -10,11 +10,6 @@ const { MongoClient, ServerApiVersion } = require('mongodb');
 const dotenv = require('dotenv');
 dotenv.config()
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
 
 
 const app = express();
