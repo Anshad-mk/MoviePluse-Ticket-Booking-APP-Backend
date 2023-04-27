cce9d6cc7cf93f61d1cb623b33b81cd3148a8715
Anshad.MK <84131708+Anshad-mk@users.noreply.github.com>
2023-04-27 12:24:56 +0530

    hosting12

--- Config.js/Razorpay.js
+++ Config.js/Razorpay.js
@@ -1,13 +1,15 @@
 const dotenv = require('dotenv');
 const Razorpay = require("razorpay");
 const shortid = require("shortid");
+YOUR_KEY_ID=rzp_test_nQfwBQkz6cfygO
+YOUR_SECRET_KEY=Q8R6KPz3a0RTgVuZZwDUzUb1
 
 // Load environment variables from .env file
 dotenv.config()
 
 const razorpay = new Razorpay({
-  key_id:process.env.YOUR_KEY_ID,
-  key_secret: process.env.YOUR_SECRET_KEY,
+  key_id:process.env.YOUR_KEY_ID || YOUR_KEY_ID,
+  key_secret: process.env.YOUR_SECRET_KEY || YOUR_SECRET_KEY,
 });
 
 const createOrder = async (amount) => {
